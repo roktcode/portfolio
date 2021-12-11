a44365f0033d5cae6e65deded64d4917fbbf4b75
muhammad <iamrokt@gmail.com>
2021-12-11 10:19:12 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector('nav');
+let navTop = nav.offsetTop;
+
+function fixedNav() {
+  if (window.scrollY >= navTop) {    
+    nav.classList.add('fixed');
+  } else {
+    nav.classList.remove('fixed');    
+  }
+}
+
+window.addEventListener('scroll', fixedNav);
+
